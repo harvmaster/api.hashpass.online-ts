cc091b356b8ec27fc8b37d10d0de2f4c875b4d00
Harvey Zuccon <hzuccon@gmail.com>
2024-03-19 18:21:12 +1100

    Added debug for users

--- src/routes/users/getAllUsers.ts
+++ src/routes/users/getAllUsers.ts
@@ -1,9 +1,18 @@
 import { Response, RequestHandler } from 'express';
 import type { AuthenticatedRequest } from '../../auth';
+import User from '../../models/User';
 
 // This route no longer returns data due to security issues
 const getAllUsers: RequestHandler = async (req: AuthenticatedRequest<{}, {}>, res: Response) => {
-  res.json([])
+  const users = await User.find({})
+  const formattedUsers = users.map(user => {
+    const { username, _id } = user
+    return {
+      username,
+      _id
+    }
+  })
+  res.json(formattedUsers)
 }
 
 export default getAllUsers;
